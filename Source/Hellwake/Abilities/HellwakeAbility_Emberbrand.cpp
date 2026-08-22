a787e81570be006d5e65777abf82f9bb4ddac6bc
gulfcoastorganics-ai <gulfcoastorganics@gmail.com>
2026-08-22 11:57:29 -0500

    Add native Emberbrand impact feedback

--- Source/Hellwake/Abilities/HellwakeAbility_Emberbrand.cpp
+++ Source/Hellwake/Abilities/HellwakeAbility_Emberbrand.cpp
@@ -1,5 +1,7 @@
 #include "Abilities/HellwakeAbility_Emberbrand.h"
 #include "AbilitySystemComponent.h"
+#include "Camera/HellwakeCameraDirector.h"
+#include "DrawDebugHelpers.h"
 #include "GameplayEffects/HellwakeGE_Damage.h"
 #include "GameplayEffects/HellwakeGE_AbilityRuntime.h"
 #include "GameFramework/Actor.h"
@@ -28,7 +30,16 @@ void UHellwakeAbility_Emberbrand::ActivateAbility(const FGameplayAbilitySpecHand
 	if (Avatar)
 	{
 		const FVector Origin = Avatar->GetActorLocation() + Avatar->GetActorForwardVector() * ForwardOffsetCm;
-		ApplyRadialDamage(Origin, RadiusCm, MinDamage, MaxDamage, DamageEffectClass, true);
+		const int32 HitCount = ApplyRadialDamage(Origin, RadiusCm, MinDamage, MaxDamage, DamageEffectClass, true);
+
+		DrawDebugCircle(GetWorld(), Origin + FVector(0.f, 0.f, 10.f), RadiusCm, 64, FColor::Orange,
+			false, 0.42f, 0, 9.f, FVector(1.f, 0.f, 0.f), FVector(0.f, 1.f, 0.f), false);
+		DrawDebugSphere(GetWorld(), Origin + FVector(0.f, 0.f, 90.f), 180.f, 18, FColor::Orange, false, 0.24f, 0, 8.f);
+
+		if (UHellwakeCameraDirector* Director = Avatar->FindComponentByClass<UHellwakeCameraDirector>())
+		{
+			Director->TriggerShake(HitCount > 0 ? 0.5f : 0.3f);
+		}
 	}
 
 	if (ASC && EmberStatusEffectClass)
